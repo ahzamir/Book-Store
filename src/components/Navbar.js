c981d041d0a667a23bafd2068e8bff0eaf8cf925
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-16 21:39:57 +0430

    add the Navbar.js component

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+  return (
+    <nav className="navBar">
+      <h1>
+        Bookstore CMS
+      </h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path} className="active-link border">
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
