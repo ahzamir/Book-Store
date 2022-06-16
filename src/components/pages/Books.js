b50d6cff3b51836a1d6f7276b164967d679f3332
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-16 21:55:24 +0430

    add a header to each component

---
+++ src/components/pages/Books.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Books = () => (<h1>Books component</h1>);
+
+export default Books;
---
+++ src/components/pages/Categories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Categories = () => (<h1>Categories component</h1>);
+
+export default Categories;
