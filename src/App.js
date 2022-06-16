5c19735a1e7d0fff530edad24ad65dd1497297b8
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-16 21:40:49 +0430

    add the Routes and navbar item to the nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './components/pages/Books';
+import Categories from './components/pages/Categories';
+
+const App = () => (
+  <>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Books />} />
+      <Route path="/calculator" element={<Categories />} />
+    </Routes>
+  </>
+);
 
 export default App;
