0db4561bc006c5cbf0e4d66d1f85e3f76535ecbc
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-22 02:29:12 +0430

    add the action creator for categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,2 +1,18 @@
 const CHECK_STATUS = 'my-app/categories/CHECK';
 
+const categories = (state = [], action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Constructuion'
+    default: return state;
+  }
+}
+
+const checkStatus = () => {
+  return {
+    type: CHECK_STATUS,
+  }
+}
+
+export { checkStatus };
+export default categories;
