c199691fcad0414b52845912923b947c5b915303
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-22 02:19:54 +0430

    write the reducer for books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,3 +2,21 @@
 const ADD_BOOK = 'my-app/books/ADD';
 const REMOVE_BOOK = 'my-app/books/REMOVE';
 
+//Reducers
+const books = (state = [], action) => {
+  switch (action.type) {
+    // do reducer stuff
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.title,
+          author: action.author
+        }
+      ];
+    case REMOVE_BOOK:
+      return state.filter(book => (book.id !== action.id));
+    default: return state;
+  }
+};
