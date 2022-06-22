1e16f565f0ab8e8a5ca6b8c6ecd2ba200355d707
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-22 20:09:46 +0430

    import the dispatch hook method on categories.js component

--- src/components/pages/CategoriesPage.js
+++ src/components/pages/CategoriesPage.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
 
-const Categories = () => (<button type="button">Check status</button>);
+const Categories = () => (
+  <button
+    type="button"
+  >Check status</button>
+);
 
 export default Categories;
