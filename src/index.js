04294c98159e00f81fc8f3f2774a393f66503283
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-22 15:04:33 +0430

    install the react-redux and import the store and provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
