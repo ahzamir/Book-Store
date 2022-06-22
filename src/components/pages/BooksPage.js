780f704308261d334a93a503a054bb5731c81d71
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-06-22 15:15:59 +0430

    import the useSelector

--- src/components/pages/BooksPage.js
+++ src/components/pages/BooksPage.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import BooksForm from '../BooksForm';
 import Books from '../Books';
+import { useSelector } from 'react-redux';
 
 const BooksPage = () => {
-  const books = [{
-    title: 'ahmad',
-    author: 'zaki',
-    id: 1,
-  },
-  {
-    title: 'zia',
-    author: 'zaki',
-    id: 2,
-  },
-  {
-    title: 'sharif',
-    author: 'zia',
-    id: 3,
-  }];
   return (
     <div className="container">
       <div className="inner">
